7ae66f1b8780bf189b48c00f41739bb5ea5e4d02
ganesh shinde <ganeshss978081@gmail.com>
2025-02-18 11:00:59 +0530

    main activity changes

--- app/src/main/java/com/learnandroid/foodorderingapp/MainActivity.java
+++ app/src/main/java/com/learnandroid/foodorderingapp/MainActivity.java
@@ -1,8 +1,12 @@
 package com.learnandroid.foodorderingapp;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -17,6 +21,8 @@ import com.learnandroid.foodorderingapp.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
+    private boolean doubleBackToExitPressedOnce = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,29 +31,83 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+
+        // Load the default fragment (Home1Fragment) initially
+        if (savedInstanceState == null) {
+            loadFramgent(new HomeFragment());
+        }
+
+
         loadFramgent(new HomeFragment());
 
         binding.bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-                int id = item.getItemId();
+                int itemId = item.getItemId();
+                Fragment selectedFragment = null;
 
-                if (id == R.id.navHome) {
-                    loadFramgent(new HomeFragment());
-                } else if (id == R.id.navOrders) {
-                    loadFramgent(new OrderFragment());
-                } else if (id == R.id.navProfile) {
-                    loadFramgent(new OrderFragment());
-                }else {
-                    loadFramgent(new HomeFragment());
+                if (itemId == R.id.navHome) {
+                    selectedFragment = getSupportFragmentManager().findFragmentByTag(HomeFragment.class.getSimpleName());
+                    if (selectedFragment == null) {
+                        selectedFragment = new HomeFragment();
+                    }
+
+                } else if (itemId == R.id.navOrders) {
+
+                    selectedFragment = getSupportFragmentManager().findFragmentByTag(OrderFragment.class.getSimpleName());
+                    if (selectedFragment == null) {
+                        selectedFragment = new OrderFragment();
+                    }
+
+                } else if (itemId == R.id.navProfile) {
+
+                    selectedFragment = getSupportFragmentManager().findFragmentByTag(OrderFragment.class.getSimpleName());
+                    if (selectedFragment == null) {
+                        selectedFragment = new OrderFragment();
+                    }
+                }  else {
+                    Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                    return false;
                 }
 
+                loadFramgent(selectedFragment);
                 return true;
+
             }
         });
 
 
+        getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+
+
+                Fragment currentFragment = getSupportFragmentManager().findFragmentById(R.id.frame);
+                // If the current fragment is not Home1Fragment, go to Home1Fragment
+                if (!(currentFragment instanceof HomeFragment)) {
+                    loadFramgent(new HomeFragment());
+                    binding.bottomNav.setSelectedItemId(R.id.navHome);  // Set the home item selected
+
+                } else {
+                    // If already in Home1Fragment, handle double back press to exit
+                    if (doubleBackToExitPressedOnce) {
+                        finish();
+                    } else {
+                        doubleBackToExitPressedOnce = true;
+                        Toast.makeText(MainActivity.this, "Press back again to exit", Toast.LENGTH_SHORT).show();
+
+                        // Reset the flag after 2 seconds
+                        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                doubleBackToExitPressedOnce = false;
+                            }
+                        }, 2000);
+                    }
+                }
+            }
+        });
     }
 
 
